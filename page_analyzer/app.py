6ea4b434277fb6ae56bb7fa0bd5de8022f060d6f
un-f0rgiven <barr85@mail.ru>
2024-10-04 12:10:42 +1000

    update file

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,11 @@
-from page_analyzer import app
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '..', 'templates'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
