45738faa12f1d07fe8bb4d54b4fd71b5245895d3
un-f0rgiven <barr85@mail.ru>
2024-10-04 12:37:38 +1000

    update file

--- page_analyzer/__init__.py
+++ page_analyzer/__init__.py
@@ -1,15 +1,13 @@
-from flask import Flask
 from dotenv import load_dotenv
 import os
+from page_analyzer.app import app
 
 load_dotenv()
 
-app = Flask(__name__)
-
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-@app.route('/')
-def home():
+@app.route('/secret')
+def secret():
     return "The key to that chain is in the bathtub"
 
 __all__ = ['app']
