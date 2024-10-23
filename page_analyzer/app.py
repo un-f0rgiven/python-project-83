09c7fa30980b563e72078b428e05c6d4fe75445f
un-f0rgiven <barr85@mail.ru>
2024-10-23 16:27:07 +1000

    update file

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -14,6 +14,12 @@ from datetime import date
 from bs4 import BeautifulSoup
 from page_analyzer.check_urls import is_valid_url, normalize_url
 from page_analyzer.requests import fetch_url
+from page_analyzer.sql_queries import (fetch_url_data,
+                                       get_urls,
+                                       insert_url,
+                                       exist_url,
+                                       get_url_name,
+                                       insert_check)
 
 
 load_dotenv()
@@ -64,39 +70,11 @@ def show_url(url_id):
             )
 
 
-def fetch_url_data(cursor, url_id):
-    cursor.execute(
-        "SELECT id, name, created_at FROM urls WHERE id = %s",
-        (url_id,)
-    )
-    url_data = cursor.fetchone()
-
-    if url_data is None:
-        return None, None
-
-    cursor.execute(
-        "SELECT id, status_code, h1, title, description, "
-        "created_at FROM url_checks WHERE url_id = %s",
-        (url_id,)
-    )
-    checks = cursor.fetchall()
-    return url_data, checks
-
-
 @app.get('/urls')
 def handle_get_request():
-
     with get_db_connection() as conn:
         with conn.cursor() as cursor:
-            cursor.execute("""
-                SELECT u.id, u.name, uc.status_code,
-                        MAX(uc.created_at) AS last_check_date
-                FROM urls u
-                LEFT JOIN url_checks uc ON u.id = uc.url_id
-                GROUP BY u.id, u.name, uc.status_code
-                ORDER BY last_check_date DESC
-            """)
-            urls = cursor.fetchall()
+            urls = get_urls(cursor)
             return render_template('list_urls.html', urls=urls)
 
 
@@ -131,28 +109,6 @@ def handle_post_request():
         return render_template('index.html', url=url), 422
 
     return render_template('index.html', url=url), 422
-
-
-def insert_url(cursor, normalized_url):
-    cursor.execute(
-        "SELECT id FROM urls WHERE name = %s",
-        (normalized_url,)
-    )
-    result = cursor.fetchone()
-
-    if result:
-        return None
-
-    cursor.execute(
-        "INSERT INTO urls (name) VALUES (%s) RETURNING id",
-        (normalized_url,)
-    )
-    return cursor.fetchone()[0]
-
-
-def exist_url(cursor, normalized_url):
-    cursor.execute("SELECT id FROM urls WHERE name = %s", (normalized_url,))
-    return cursor.fetchone()
 
 
 @app.route('/urls/<int:url_id>/checks', methods=['POST'])
@@ -209,36 +165,3 @@ def parse_html(html):
     )
 
     return h1_content, title_content, description_content
-
-
-def get_url_name(cursor, url_id):
-    cursor.execute("SELECT name FROM urls WHERE id = %s", (url_id,))
-    return cursor.fetchone()
-
-
-def insert_check(
-        cursor,
-        url_id,
-        status_code,
-        h1_content,
-        title_content,
-        description_content,
-        created_at
-):
-    try:
-        cursor.execute(
-            "INSERT INTO url_checks "
-            "(url_id, status_code, h1, title, description, created_at) "
-            "VALUES (%s, %s, %s, %s, %s, %s)",
-            (
-                url_id,
-                status_code,
-                h1_content,
-                title_content,
-                description_content,
-                created_at
-            )
-        )
-    except Exception as e:
-        logging.error(f"Ошибка при вставке проверки: {str(e)}")
-        raise
--- page_analyzer/sql_queries.py
+++ page_analyzer/sql_queries.py
@@ -25,6 +25,89 @@ def execute_sql_file(filename):
             conn.commit()
 
 
+def fetch_url_data(cursor, url_id):
+    cursor.execute(
+        "SELECT id, name, created_at FROM urls WHERE id = %s",
+        (url_id,)
+    )
+    url_data = cursor.fetchone()
+
+    if url_data is None:
+        return None, None
+
+    cursor.execute(
+        "SELECT id, status_code, h1, title, description, "
+        "created_at FROM url_checks WHERE url_id = %s",
+        (url_id,)
+    )
+    checks = cursor.fetchall()
+    return url_data, checks
+
+
+def get_urls(cursor):
+    cursor.execute("""
+        SELECT u.id, u.name, uc.status_code,
+        MAX(uc.created_at) AS last_check_date
+        FROM urls u
+        LEFT JOIN url_checks uc ON u.id = uc.url_id
+        GROUP BY u.id, u.name, uc.status_code
+        ORDER BY last_check_date DESC
+    """)
+    urls = cursor.fetchall()
+    return urls
+
+
+def insert_url(cursor, normalized_url):
+    cursor.execute(
+        "SELECT id FROM urls WHERE name = %s",
+        (normalized_url,)
+    )
+    result = cursor.fetchone()
+
+    if result:
+        return None
+
+    cursor.execute(
+        "INSERT INTO urls (name) VALUES (%s) RETURNING id",
+        (normalized_url,)
+    )
+    return cursor.fetchone()[0]
+
+
+def exist_url(cursor, normalized_url):
+    cursor.execute("SELECT id FROM urls WHERE name = %s", (normalized_url,))
+    return cursor.fetchone()
+
+
+def get_url_name(cursor, url_id):
+    cursor.execute("SELECT name FROM urls WHERE id = %s", (url_id,))
+    return cursor.fetchone()
+
+
+def insert_check(
+        cursor,
+        url_id,
+        status_code,
+        h1_content,
+        title_content,
+        description_content,
+        created_at
+):
+    cursor.execute(
+        "INSERT INTO url_checks "
+        "(url_id, status_code, h1, title, description, created_at) "
+        "VALUES (%s, %s, %s, %s, %s, %s)",
+        (
+            url_id,
+            status_code,
+            h1_content,
+            title_content,
+            description_content,
+            created_at
+        )
+    )
+
+
 if __name__ == '__main__':
     sql_file_path = os.getenv('SQL_FILE_PATH')
     if not sql_file_path:
